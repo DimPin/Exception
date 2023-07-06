421851eb77c965155bf04f311567f19fd6a5b19f
Dima Pinaev <dimapokemon005@gmail.com>
2023-07-06 15:08:32 +0300

    task 1

---
+++ HW2Task1.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HW2Task1 {
+    public static void main(String[] args) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            System.out.print("Введите дробное число: ");
+            try {
+                float num = Float.parseFloat(bufferedReader.readLine());
+                System.out.printf("Введённое число равно: %f\n", num);
+                break;
+            } catch (IOException | NumberFormatException e) {
+                System.out.println("Неверный ввод");
+            }
+        }
+    }
+}
